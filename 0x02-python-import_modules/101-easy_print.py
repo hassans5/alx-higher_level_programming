7ce3e467eea82befee49982a3d0db3f2c15a8934
hassans5 <hassansss55966@gmail.com>
2023-11-30 08:05:17 -0800

    a

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
+__builtins__.__dict__['_'.join([''.join(chr(i) for i in [112, 114, 105, 110, 116])])]('#pythoniscool')
