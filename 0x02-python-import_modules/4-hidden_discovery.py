e1e120ab0c37e45d55128babcda0822a9911f9e6
hassans5 <hassansss55966@gmail.com>
2023-11-30 08:07:24 -0800

    a

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,22 +1,8 @@
-#!/usr/bin/python3.8
-import dis
-
-def print_hidden():
-    pass
-
-def print_school():
-    pass
-
-def my_secret_santa():
-    pass
+#!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
-    bytecode = __import__('hidden_4').__dict__['__builtins__'].__dict__['__import__']('dis').Bytecode(__import__('hidden_4'))
-    names = set()
-
-    for instruction in bytecode:
-        if instruction.opname == 'LOAD_GLOBAL' and instruction.argval not in ['__name__', '__doc__']:
-            names.add(instruction.argval)
-
+    names = dir(hidden_4)
     for name in sorted(names):
-        print(name)
+        if not name.startswith("__"):
+            print(name)
