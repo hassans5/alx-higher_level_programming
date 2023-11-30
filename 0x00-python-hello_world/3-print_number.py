7ccac47c6a45dc75f7e1ebddd3b59c8adcf90330
hassans5 <hassansss55966@gmail.com>
2023-11-30 08:53:21 -0800

    a

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -4,4 +4,4 @@ Prints the integer stored in the variable number,
 followed by Battery street, followed by a new line.
 """
 number = 98
-print(f"{number} Battery street")
+print("%d Battery street" % number)
