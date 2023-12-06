07ede5c148caf0e1191919dad826568cbf6f64d9
hassans5 <hassansss55966@gmail.com>
2023-12-06 03:24:49 -0800

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(row):  # Iterate over elements in the row
-            new_matrix[idx][idx2] = col ** 2
-    return new_matrix  
+
+    squared = []
+
+    for line in matrix:
+
+        squared.append([c**2 for c in line])
+
+    return squared  
